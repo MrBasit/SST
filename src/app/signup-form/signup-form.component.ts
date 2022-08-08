f818649b1b9350844c22a51817f375c3c79f6475
Sahbaan Alam <sahbaanalam34@gmail.com>
2022-08-08 14:02:10 +0500

    OnModuleChange has been added to email and username component

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -11,8 +11,12 @@ import { UserService } from '../user.service';
 })
 export class SignupFormComponent implements OnInit {
   isLoading:boolean=false;
+  isUsername:boolean=false;
+  AboveError:boolean=false;
+  isEmail:boolean=false;
   isSignupSuccessfull=false;
   Error:any=null;
+  Error2:any=null;
   constructor(public http:HttpClient,public router:Router,public userService:UserService) { }
   signupForm=new FormGroup({
     firstnameFormControl:new FormControl('',[Validators.required]),
@@ -39,34 +43,68 @@ export class SignupFormComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  onSubmit(){
-    if(this.signupForm.valid){
-      this.isLoading=true;
-      console.log(this.signupForm.value);
-      let url="https://calm-hamlet-62154.herokuapp.com/user/signup";
+
+  checkemail(){
+    console.log(this.signupForm.value);
+      let url="https://calm-hamlet-62154.herokuapp.com/user/checkemail";
       let body={
-          username:this.signupForm.value['nameFormControl'],
-          firstName:this.signupForm.value['firstnameFormControl'],
-          lastName:this.signupForm.value['lastnameFormControl'],
-          email:this.signupForm.value['emailFormControl'],
-          password:this.signupForm.value['passwordFormControl'],
+          email:this.signupForm.value['emailFormControl']
       }
       console.log('body -> ',body);
       this.userService.Signup(url,body).subscribe(
         (r:any)=>{
             this.isLoading=false;
-            if(r.responseCode!=1){
-            this.Error={
-              error:{
-                error:r.responseMessage
+            this.isEmail=r;
+            if (this.isEmail) {
+              this.Error2={
+                error:{
+                  error:"Email Already Exists"
+                }
+              }
+            }else{
+              this.Error2={
+                error:{
+                  error:""
+                }
               }
             }
-          }
-          else{
-            this.Error=null;
-            console.log(r);
-            this.isSignupSuccessfull=true;
-          }        
+        },
+        e=>{
+          this.isLoading=false;
+          console.log('error => ',e)
+          this.Error=e;
+        }
+        
+      )
+      
+
+  }
+
+  checkUsername(){
+    console.log(this.signupForm.value);
+      let url="https://calm-hamlet-62154.herokuapp.com/user/checkusername";
+      let body={
+          username:this.signupForm.value['nameFormControl']
+      }
+      console.log('body -> ',body);
+      this.userService.Signup(url,body).subscribe(
+        (r:any)=>{
+            this.isLoading=false;
+            this.isUsername=r;
+            if (this.isUsername) {
+              this.Error={
+                error:{
+                  error:"User is already registered with this Username! Try a Different One"
+                }
+              }
+            }else{
+              this.Error={
+                error:{
+                  error:""
+                }
+              }
+            }
+             
         },
         e=>{
           this.isLoading=false;
@@ -74,11 +112,57 @@ export class SignupFormComponent implements OnInit {
           this.Error=e;
         }
       )
+      
 
+  }
+  onSubmit(){
+
+    if(!this.isUsername && !this.isEmail){
+      this.AboveError=false
+      if(this.signupForm.valid){
+        this.isLoading=true;
+        console.log(this.signupForm.value);
+        let url="https://calm-hamlet-62154.herokuapp.com/user/signup";
+        let body={
+            username:this.signupForm.value['nameFormControl'],
+            firstName:this.signupForm.value['firstnameFormControl'],
+            lastName:this.signupForm.value['lastnameFormControl'],
+            email:this.signupForm.value['emailFormControl'],
+            password:this.signupForm.value['passwordFormControl'],
+        }
+        console.log('body -> ',body);
+        this.userService.Signup(url,body).subscribe(
+          (r:any)=>{
+              this.isLoading=false;
+              if(r.responseCode!=1){
+              this.Error={
+                error:{
+                  error:r.responseMessage
+                }
+              }
+            }
+            else{
+              this.Error=null;
+              console.log(r);
+              this.isSignupSuccessfull=true;
+            }        
+          },
+          e=>{
+            this.isLoading=false;
+            console.log('error => ',e)
+            this.Error=e;
+          }
+        )
+  
+      }
+      else{
+        console.log('invalid form')
+      }
+    }else{
+      this.AboveError=true;
     }
-    else{
-      console.log('invalid form')
-    }
+
+    
 
   }
 
